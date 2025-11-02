d887bd5cd8722b75db94f129bbdb69456a327af1
Rahul Roy <rahulroyapd80@gmail.com>
2025-11-02 21:15:37 +0530

    feat: add multiple themes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,31 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { Toaster } from 'sonner';
 import App from './app';
+import { ModeProvider } from './components/app/logic/themes/mode';
+
+import './index.css';
+import './themes/default.theme.css';
+import './themes/blue.theme.css';
+import './themes/green.theme.css';
+import './themes/rose.theme.css';
+import './themes/violet.theme.css';
+import { ThemeProvider } from './components/app/logic/themes/theme';
 
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <App />
-      <Toaster
-        closeButton
-        position='bottom-right'
-        richColors
-      />
+      <ModeProvider>
+        <ThemeProvider>
+          <App />
+          <Toaster
+            closeButton
+            position='bottom-right'
+            richColors
+          />
+        </ThemeProvider>
+      </ModeProvider>
     </StrictMode>
   );
 }
